db2f7e120cbd4e7cfec916b49362b27a3f97d38f
Rootselection <48713457+Rootselection@users.noreply.github.com>
2021-03-03 12:00:22 +0200

    update ver

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log('file 1');
+var images = document.querySelectorAll(img);
+new simpleParallax(images);
+
+$('.your-class').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
